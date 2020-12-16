973cd2fe9219d940adba1f1b287a2978433317b4
fabienbounoir <fabien.bounoir@epitech.eu>
2020-12-17 00:11:01 +0100

    rendu frontend

---
+++ assets/js/connexion.js
@@ -0,0 +1,86 @@
+const url = "http://localhost:3000"  
+
+const connexionUrl = "/login"       //Chemin pour tester si l'utilisateur peut se connecter
+
+//Verifie si la connexion est OK
+var Connexion = function(){
+
+    console.log("je suis la")
+
+    if($("#inputIdentifiant").val() != "" && $("#inputPassword").val() != "")
+    {
+        const connexion = {
+            "userName": $("#inputIdentifiant").val(),
+            "password": $("#inputPassword").val() ,
+        }
+
+        var settings = {
+            "url": url + connexionUrl,
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "data": JSON.stringify(connexion),
+        };
+          
+        $.ajax(settings).done(function (response) {
+            console.log(response)
+            if(response.code == "200")
+            {   
+
+                setCookie("nom",response.rowse[0].nom,3);
+                setCookie("prenom",response.rowse[0].prenom,3);
+                setCookie("email",response.rowse[0].mail,3);
+                setCookie("token",response.token,3);
+                setCookie("username",response.rowse[0].username,3);
+
+                document.location.href="index.html";
+
+            }
+            else if(response.code == "400")
+            {
+                $(".connexionInfo").html('<p class="text-center" ><code>'+response.erreur+'</code></p>');
+            }
+        });
+    }
+    else
+    {
+        $(".connexionInfo").html('<p class="text-center"><code>⟼ Un champ et incorrect ⟻</code></p>');
+    }
+}
+
+//crée cookie
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+//supprimer cookie de connexion en le ciblant par son nom
+function rmCookie(name){
+    document.cookie = name + "=" + ";"  + 'expires=Thu, 01 Jan 1970 00:00:00 UTC' + "; path=/";
+}
+
+//recuperer valeur cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+$(document).ready(function() {
+
+    $("#buttonConnexion").click(() =>{
+        Connexion()
+    })
+})
---
+++ assets/js/register.js
@@ -0,0 +1,55 @@
+const url = "http://localhost:3000"     //server Adresse + port
+
+const registerUrl = "/register"         //Chemin pour s'enregistrer
+
+//function qui verifie que le formulaire et valide
+function VerifRegister(){
+
+    //test si les champ Entrer sont valides
+    if(checkEmail($("#inputEmail").val()) && $("#inputFirstName").val().trim() != "" && $("#inputLastName").val().trim() != "" && $("#inputUsername").val().trim() != "" && $("#inputPassword").val().trim() != "" && $("#inputPassword").val() == $("#inputConfirmPassword").val())
+    {
+        const user = {
+            "prenom": $("#inputFirstName").val().trim(),
+            "nom": $("#inputLastName").val().trim(),
+            "email": $("#inputEmail").val(),
+            "tel": $("#inputPhone").val(),
+            "userName": $("#inputUsername").val(),
+            "password": $("#inputPassword").val(),
+        }
+
+        var settings = {
+            "url": url + registerUrl,
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "data": JSON.stringify(user),
+        };
+          
+        $.ajax(settings).done(function (response) {
+            console.log(response)
+            if(response.code == "200")
+            {   
+                document.location.href="login.html";
+            }
+        });
+    }
+    else
+    {
+        $("#messageRegister").html('<p class="text-center"><code>⟼ Un champ et incorrect ⟻</code></p>');
+    }
+}
+
+//Test si l'email rentré est valide
+function checkEmail(email) {
+    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+}
+
+
+$(document).ready(function() {
+
+    $("#buttonRegister").click(() =>{
+        VerifRegister()
+    })
+})
